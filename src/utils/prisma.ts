f42ddac878397e66975c2092e45e67e3b34cbd97
satya indra <satyaindra@satyas-MacBook-Air.local>
2026-03-09 21:38:04 +0530

    ..

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -4,4 +4,4 @@ import { Pool } from 'pg';
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter } as any);
